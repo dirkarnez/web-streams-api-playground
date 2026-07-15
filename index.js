23c4db0d51fc0e3557933c90010acf45bb617f8f
Dirk Arnez <smalldirkalex@gmail.com>
2026-07-15 16:47:28 +0800

    - add index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const stream = new TextDecoderStream();
+
+// Write data to be decoded
+const data = new Uint8Array([228, 189, 160, 229, 165, 189, 228, 184, 150, 231, 149 /*, 140*/]);  //comment out 140 to simulate incomplete stream
+const writer = stream.writable.getWriter();
+writer.write(data);
+writer.close();
+
+// Read decoded data
+const reader = stream.readable.getReader();
+let done = false;
+let output = "";
+while (!done) {
+  const result = await reader.read();
+  if (result.value) {
+    output += result.value;
+  }
+  done = result.done;
+}
+console.log(output); // 你好世(界)
